8ab205bff06e004a2a026b42d5cae5da6184fc38
neinsdrc <1136148849@qq.com>
2020-09-12 12:24:45 +0800

    first commit

---
+++ src/main/java/com/buaa/cmsys/CmsysApplication.java
@@ -0,0 +1,13 @@
+package com.buaa.cmsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsysApplication.class, args);
+    }
+
+}
